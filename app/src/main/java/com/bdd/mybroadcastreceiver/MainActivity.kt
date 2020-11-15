118943509e302c89fc2e40bde730dd440431fcf6
Rizky Gumilang <rizkygum@gmail.com>
2020-11-16 01:30:11 +0700

    Initial commit

---
+++ app/src/main/java/com/bdd/mybroadcastreceiver/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.bdd.mybroadcastreceiver
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.util.Log
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        private const val SMS_REQUEST_CODE = 101
+        const val ACTION_DOWNLOAD_STATUS = "download_status"
+    }
+
+    private lateinit var downloadReceiver: BroadcastReceiver
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_permission.setOnClickListener(this)
+        btn_download.setOnClickListener(this)
+
+        downloadReceiver = object : BroadcastReceiver(){
+            override fun onReceive(context: Context?, intent: Intent?) {
+                Log.d(DownloadService.TAG, "Download Selesai")
+                Toast.makeText(context, "Download Selesai", Toast.LENGTH_SHORT).show()
+            }
+        }
+        val downloadIntentFilter = IntentFilter(ACTION_DOWNLOAD_STATUS)
+
+        registerReceiver(downloadReceiver, downloadIntentFilter)
+
+    }
+
+    override fun onClick(v: View?) {
+        when{
+            v?.id == R.id.btn_permission -> PermissionManager.check(this, Manifest.permission.RECEIVE_SMS, SMS_REQUEST_CODE)
+            v?.id == R.id.btn_download -> {
+                val downloadServiceIntent = Intent(this, DownloadService::class.java)
+                startService(downloadServiceIntent)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == SMS_REQUEST_CODE){
+            when {
+                grantResults[0] == PackageManager.PERMISSION_GRANTED -> Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show()
+                else -> Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(downloadReceiver)
+    }
+}
